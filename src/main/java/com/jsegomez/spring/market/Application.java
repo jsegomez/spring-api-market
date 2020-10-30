135f6fb4d28cc5bf16ceb55b2a90bec9eb3c434c
jsegomez <jsegomez06@gmail.com>
2020-10-29 23:55:49 -0600

    Inicio del proyecto Market

---
+++ src/main/java/com/jsegomez/spring/market/Application.java
@@ -0,0 +1,13 @@
+package com.jsegomez.spring.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
